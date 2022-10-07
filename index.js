8051ab9eff864b9274f49a116dda764bb6490b6e
dabit3 <dabit3@gmail.com>
2022-10-07 00:02:49 -0500

    changed provider

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { ethers } from 'ethers'
 import ABI from './ABI.json' assert { type: "json" }
 
-const handles = ["stani.lens", "paris.lens", "nader.lens"]
+const handles = ["stani.lens", "paris.lens", "nader.lens", "camiinthisthang.lens"]
 
 const ENDPOINT = process.env.infuraEndpoint
 const CONTRACT_ADDRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d"
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcBatchProvider(ENDPOINT)
+  const provider = new ethers.providers.JsonRpcProvider(ENDPOINT)
   const signer = await getSigner(provider)
   await followProfiles(provider, signer)
 }
